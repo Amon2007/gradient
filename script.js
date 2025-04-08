677a0ecc3c377852713d0748f5da102056bdfe05
Amon2007 <sefanlevchyk@gmail.com>
2025-04-08 14:36:28 +0300

    v.1.2

--- script.js
+++ script.js
@@ -2,23 +2,20 @@
 let mouseCursor = document.querySelector(".cursor");
 let navLinks = document.querySelectorAll("h1, h2, button, a, p");
 
-window.addEventListener("mousemove", cursor, { passive: true });
+window.addEventListener("mousemove", cursor);
 
 function cursor(e) {
   mouseCursor.style.top = e.pageY + "px";
   mouseCursor.style.left = e.pageX + "px";
 }
 
-navLinks.forEach(
-  (link) => {
-    link.addEventListener("mouseleave", () => {
-      mouseCursor.classList.remove("link-grow");
-      link.classList.remove("hovered-link");
-    });
-    link.addEventListener("mousemove", () => {
-      mouseCursor.classList.add("link-grow");
-      link.classList.add("hovered-link");
-    });
-  },
-  { passive: true }
-);
+navLinks.forEach((link) => {
+  link.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("link-grow");
+    link.classList.remove("hovered-link");
+  });
+  link.addEventListener("mousemove", () => {
+    mouseCursor.classList.add("link-grow");
+    link.classList.add("hovered-link");
+  });
+});
