9f22b3827fb67879174491ef421ea0a5f86f7056
Amon2007 <sefanlevchyk@gmail.com>
2025-04-09 11:49:51 +0300

    Improved HTML structure and added icons... V.2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Cursor 2
 let mouseCursor = document.querySelector(".cursor");
-let navLinks = document.querySelectorAll("h1, h2, button, a, p");
+let navLinks = document.querySelectorAll(
+  "h1, h2, button, a, p, .nav-list a, .btn-get-started, .btn-signing-main"
+);
 
 window.addEventListener("mousemove", cursor);
 
@@ -18,4 +20,14 @@ navLinks.forEach((link) => {
     mouseCursor.classList.add("link-grow");
     link.classList.add("hovered-link");
   });
+});
+
+// BLUR FOR NAV DURING SCROLL
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 });
